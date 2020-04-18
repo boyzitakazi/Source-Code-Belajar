176c2bb352a305b53ec9b71cb532dc4edb280397
Boyzi Takazi <52165650+boyzitakazi@users.noreply.github.com>
2020-04-18 14:03:46 +0700

    Update mengakses_file_external.cpp

--- CPP Course/Eps25 Menulis File Eksternal/mengakses_file_external.cpp
+++ CPP Course/Eps25 Menulis File Eksternal/mengakses_file_external.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 // mengakses data eksternal ifstream  | >>
 // membuat data ke eksternal ofstream | <<
-// mengenal eof (external output file)
+// mengenal eof (external output file) | untuk menampilkan semua data
 
 
 int main(){
@@ -14,16 +14,22 @@ int main(){
 	ifstream myFile;
 	string access_member;
 	string name;
-
+	
+	// buka file, jika belum ada tambahkan menggunakan ios::trunc
 	myFile.open("data.txt");
+	// looping file
 	while(!false){
 		getline(myFile, access_member);
+		// jika menemukan data siswa maka akan berenti
 		if (access_member == "data siswa"){
 			break;
 		}
 	}
 
+	// looping data menggunakan eof yang dimana eof ini akan mengambil semua data
+	// yang ada di file data.txt
 	while(!myFile.eof()){
+		// menggunakan metode string yaitu getline
 		getline(myFile, access_member);
 		cout << access_member << endl;
 	}
